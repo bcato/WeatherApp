abcdac0111e53347039ce62da1aed5483860f50d
Brandon Cato <brandoncato@Brandons-MacBook-Air-2.local>
2015-01-29 17:44:36 -0800

    cleaned up ViewController code

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -31,7 +31,6 @@ class ViewController: UIViewController {
                 
                 let currentWeather = Current(weatherDictionary: weatherDictionary)
                 
-                println(currentWeather.currentTime!)
                 
             }
             
