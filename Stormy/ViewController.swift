ccc5e3f9f288e02b554781d73d0beee8a3d25a2d
Brandon Cato <brandoncato@Brandons-MacBook-Air-2.local>
2015-01-30 15:50:38 -0800

    built the viewController to get labels to dynamic update from JSON data pull

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -10,13 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    @IBOutlet weak var iconView: UIImageView!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    @IBOutlet weak var precipitationLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    @IBOutlet weak var refreshActivityIndicator: UIActivityIndicatorView!
+    
+    
     private let apiKey = "2fea654da11f5cdac51abb5fef844cfb"
     private let locationPoints = "37.8267/-122.423"
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        refreshActivityIndicator.hidden = true
+        getCurrentWeatherData()
+    }
+    
+    func getCurrentWeatherData() -> Void {
         let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
         let encodedPoints = locationPoints.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
         
@@ -24,12 +41,26 @@ class ViewController: UIViewController {
         
         let sharedSession = NSURLSession.sharedSession()
         let downloadTask: NSURLSessionDownloadTask = sharedSession.downloadTaskWithURL(forecastURL!, completionHandler: { (location: NSURL!, response: NSURLResponse!, error: NSError!) -> Void in
-
+            
             if (error == nil) {
                 let dataObject = NSData(contentsOfURL: location)!
                 let weatherDictionary: NSDictionary = NSJSONSerialization.JSONObjectWithData(dataObject, options: nil, error: nil) as NSDictionary
                 
                 let currentWeather = Current(weatherDictionary: weatherDictionary)
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.temperatureLabel.text = "\(currentWeather.temperature)"
+                    self.iconView.image = currentWeather.icon!
+                    self.currentTimeLabel.text = "At \(currentWeather.currentTime!) it is..."
+                    self.humidityLabel.text = "\(currentWeather.precipProbability)"
+                    self.precipitationLabel.text = "\(currentWeather.precipProbability)"
+                    self.summaryLabel.text = "\(currentWeather.summary)"
+                    
+                    //Stop refresh animation
+                    self.refreshActivityIndicator.stopAnimating()
+                    self.refreshActivityIndicator.hidden = true
+                    self.refreshButton.hidden = false
+                })
                 
                 
             }
@@ -38,8 +69,18 @@ class ViewController: UIViewController {
         })
         
         
-            downloadTask.resume()
+        downloadTask.resume()
     }
+    
+    @IBAction func refresh() {
+        
+        getCurrentWeatherData()
+        
+        refreshButton.hidden = true
+        refreshActivityIndicator.hidden = false
+        refreshActivityIndicator.startAnimating()
+    }
+    
 
 }
 
